1d4b08e51adbce884ac69cf61df5c72a8a21db6b
jd2602 <josuefree2602@gmail.com>
2024-02-29 02:37:40 +0100

    Gradiente, Energy

--- code.py
+++ code.py
@@ -74,7 +74,7 @@ def GetDomain(A, b, K):
 def SolvePDE01(domain):
 	# solve PDE
 	V = fem.FunctionSpace(domain, ("Lagrange", 1))
-	f = fem.Constant(domain, default_scalar_type(-10.))
+	f = fem.Constant(domain, default_scalar_type(10.))
 	# or
 	'''
 	x = ufl.SpatialCoordinate(domain)
@@ -85,7 +85,7 @@ def SolvePDE01(domain):
 	f = -10. * ufl.exp(-beta*((x[0]-C0)**2 + (x[1]-C1)**2 + (x[2]-C2)**2))
 	'''
 	uD = fem.Function(V)
-	uD.interpolate(lambda x: x[0]*0)
+	uD.interpolate(lambda x: x[2]*x[1]*x[0]*0)
 	domain.topology.create_connectivity(d0 = 2, d1 = 3)
 	boundary_facets = mesh.exterior_facet_indices(domain.topology)
 	boundary_dofs = fem.locate_dofs_topological(V, 2, boundary_facets)
@@ -96,21 +96,67 @@ def SolvePDE01(domain):
 
 	a = ufl.dot(ufl.grad(u), ufl.grad(v)) * ufl.dx
 	L = f * v * ufl.dx
-	problem = LinearProblem(a, L, bcs=[bc], petsc_options={"ksp_type": "preonly", "pc_type": "lu"})
+	problem = LinearProblem(a, L, bcs = [bc], petsc_options={"ksp_type": "preonly", "pc_type": "lu"})
 	uh = problem.solve()
 
-	return V, uh
+	W = fem.VectorFunctionSpace(domain, ("Lagrange", 1))
+	w = ufl.TrialFunction(W)
+	v = ufl.TestFunction(W)
 
-def PlotSolution(V, uh):
-	topology, cell_types, x = vtk_mesh(V)
-	grid = pyvista.UnstructuredGrid(topology, cell_types, x)
-	# plot
-	grid.point_data["u"] = uh.x.array
-	print(uh.x.array)
-	warped = grid.warp_by_scalar("u", factor=1)
+	a = ufl.inner(w, v) * ufl.dx
+	L = ufl.inner(ufl.grad(uh), v) * ufl.dx
+	problem = LinearProblem(a, L)
+	Guh = problem.solve()
+
+	n = ufl.FacetNormal(domain)
+	flux1 = ufl.dot(ufl.grad(uh), n)**2 * ufl.ds
+	flux2 = ufl.dot(Guh, n)**2 * ufl.ds
+	total_flux1 = fem.assemble_scalar(fem.form(flux1))
+	total_flux2 = fem.assemble_scalar(fem.form(flux2))	
+	print('Total :', total_flux1, total_flux2)
+	return V, uh, W, Guh
+
+def PlotSolution(V, uh, W, Guh, domain):
+	
 	plotter = pyvista.Plotter()
-	plotter.add_mesh(warped, show_edges=True, show_scalar_bar=True, scalars="u")
+	grid = pyvista.UnstructuredGrid(*vtk_mesh(V))
+	grid.point_data["u"] = uh.x.array
+	grid.set_active_scalars("u")
+	warp = grid.warp_by_scalar("u", factor=1)
+	actor = plotter.add_mesh(warp, show_edges=True)
 	plotter.show()
+	
+	plotter = pyvista.Plotter()
+	plotter.set_position([0, 0, 5])
+
+	# We include ghosts cells as we access all degrees of freedom (including ghosts) on each process
+	top_imap = domain.topology.index_map(domain.topology.dim)
+	num_cells = top_imap.size_local + top_imap.num_ghosts
+	midpoints = mesh.compute_midpoints(domain, domain.topology.dim, range(num_cells))
+
+	num_dofs = W.dofmap.index_map.size_local + W.dofmap.index_map.num_ghosts
+	assert (num_cells == num_dofs)
+	values = np.zeros((num_dofs, 3), dtype=np.float64)
+	values[:, :domain.geometry.dim] = Guh.x.array.real.reshape(num_dofs, W.dofmap.index_map_bs)
+	cloud = pyvista.PolyData(midpoints)
+	cloud["Gu"] = values
+	glyphs = cloud.glyph("Gu", factor=0.05)
+	actor = plotter.add_mesh(grid, style="wireframe", color="k")
+	actor2 = plotter.add_mesh(glyphs)
+
+	plotter.show()
+
+def Energy(u):
+	# return the energy of the approximate solution
+	energy = .5*ufl.dot(ufl.grad(u), ufl.grad(u))*ufl.dx
+	return fem.assemble_scalar(fem.form(energy))
+
+def GetGrad(domain, uh):
+	W = fem.VectorFunctionSpace(domain, ("DG", 0))
+	Guh = fem.Function(W)
+	G_exp = fem.Expression(ufl.grad(uh), W.element.interpolation_points())
+	Guh.interpolate(G_exp)
+	return Guh
 
 def example():
 	# Ax <= b
@@ -126,8 +172,21 @@ def example():
 	A, b = normalize(A, b)
 	
 	domain = GetDomain(A, b, 8)
-	V, uh = SolvePDE01(domain)
-	PlotSolution(V, uh)
+	V, uh, W, Guh = SolvePDE01(domain)
+
+	print('Energy = ', Energy(uh))
+	
+	p = pyvista.Plotter()
+	topology, cell_types, geometry = vtk_mesh(V)
+	grid = pyvista.UnstructuredGrid(topology, cell_types, geometry)
+	grid["Gu"] = Guh.x.array.reshape((geometry.shape[0], 3))
+	actor_0 = p.add_mesh(grid, style="wireframe", color="k")
+	warped = grid.warp_by_vector("Gu", factor=0.01)
+	actor_1 = p.add_mesh(warped, show_edges=True)
+	p.show_axes()
+	p.show()
+	#https://jsdokken.com/dolfinx-tutorial/chapter3/robin_neumann_dirichlet.html
+	#PlotSolution(V, uh, W, Guh, domain)
 	
 	
 example()	
